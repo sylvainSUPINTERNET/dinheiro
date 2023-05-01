5a23221b7cd45a9d7f4b822756fc583a4018283a
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-05-01 20:31:07 +0200

    [OK]

--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,19 @@
+import { Button } from '@chakra-ui/react';
 import Link from 'next/link';
+import { FaBars } from 'react-icons/fa';
 
-const Header = () => {
+import Image from "next/image";
+
+const Header = ({onOpen, btnRef}: {onOpen:any, btnRef:any}) => {
   return (
-    <header className="bg-red-900 p-4 text-4xl border-b-2 border-black border-solid">
-      <nav>
-        <ul>
-          <li className='flex justify-center'>
-            <Link href="/">
-              Dinheiro
-            </Link>
-          </li>
-        </ul>
+    <header className="bg-gradient-to-b from-purple-500 to-blue-500 p-4 text-4xl border-b-2 border-black border-solid shadow-lg">
+      <nav className="flex items-center justify-between">
+        
+        <div ref={btnRef} onClick={onOpen}>
+          <FaBars className="text-white cursor-pointer" />
+        </div>
+
+        <Link href="/" className="text-white text-center flex-1 text-6xl font-bold">Dinheiro</Link>
       </nav>
     </header>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,18 @@
 // import { Inter } from 'next/font/google'
 
 import Header from '@/components/header'
-import PictureGallery from '@/components/user/PictureGallery.component';
 import { supabase } from '@/lib/supabaseClient'
+import { useDisclosure, Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Input,
+  Button,} from '@chakra-ui/react';
 import { useEffect, useRef, useState } from 'react';
-
-
-import { Swiper } from 'swiper';
+import Image from "next/image";
 
 
 export async function getServerSideProps() {
@@ -23,6 +29,9 @@ export async function getServerSideProps() {
 
 export default function Home({ countries }: {countries: any}) {
   
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = useRef()
+
   useEffect(() => {
 
   }, []);
@@ -31,7 +40,37 @@ export default function Home({ countries }: {countries: any}) {
   return (
     <main className="bg-gradient-to-b from-blue-500 to-purple-500 h-screen">
 
-      <Header/>
+
+      <Drawer
+              isOpen={isOpen}
+              placement='left'
+              onClose={onClose}
+              finalFocusRef={btnRef}
+            >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+
+          
+          <div className="flex items-center mt-5 p-4">
+            <div className="cursor-pointer transition duration-300 ease-in-out hover:scale-110 mr-4">
+              <Image src="https://picsum.photos/200" width={76} height={76 } alt={'profil'} className="rounded-full border-2 border-solid border-black" />
+            </div>
+            <h1 className="text-black text-2xl font-bold">Sylvain</h1>
+          </div>
+
+          <DrawerHeader>Conversation</DrawerHeader>
+          
+
+          <DrawerHeader>Match</DrawerHeader>
+
+          <DrawerHeader>Historique</DrawerHeader>
+
+
+        </DrawerContent>
+      </Drawer>
+
+      <Header onOpen={onOpen} btnRef={btnRef}/>
 
       <div className='md:container md:mx-auto md:mt-20'>
         <div className="flex flex-wrap justify-center">
@@ -39,9 +78,9 @@ export default function Home({ countries }: {countries: any}) {
         <div className="relative h-[20em] w-full md:h-[40em] md:w-[40em] mt-5 mb-5 md:mt-0 md:mb-0">
           <img className="border-2 border-black border-solid absolute top-0 left-0 w-full h-full object-cover md:rounded-tl-lg md:rounded-bl-lg shadow-2xl" src="https://media.discordapp.net/attachments/997271582005264384/1102607582268493924/ricsoft74_view_from_an_elevated_building_Mexico_City_in_the_fut_e8616032-4312-488f-8c43-52f7526a9c32.png?width=984&height=656" alt="your-image-alt" />
 
-          <button className="absolute bottom-[-75px] md:left-40 left-20 transform -translate-y-1/2 py-6 px-8 bg-gradient-to-br from-blue-400 to-purple-500 hover:from-purple-500 hover:to-blue-400 text-white font-bold rounded-full shadow-lg text-2xl transition duration-300 ease-in-out hover:scale-110" >X</button>
+          <button className="border-2 border-black border-solid absolute bottom-[-75px] md:left-40 left-20 transform -translate-y-1/2 py-6 px-8 bg-gradient-to-br from-blue-400 to-purple-500 hover:from-purple-500 hover:to-blue-400 text-white font-bold rounded-full shadow-lg text-2xl transition duration-300 ease-in-out hover:scale-110" >X</button>
 
-          <button className="absolute bottom-[-75px] md:right-40 right-20  transform -translate-y-1/2  py-6 px-8 bg-gradient-to-br from-blue-400 to-purple-500 hover:from-purple-500 hover:to-blue-400  text-white font-bold rounded-full shadow-lg text-2xl transition duration-300 ease-in-out hover:scale-110">✓</button>
+          <button className="border-2 border-black border-solid absolute bottom-[-75px] md:right-40 right-20  transform -translate-y-1/2  py-6 px-8 bg-gradient-to-br from-blue-400 to-purple-500 hover:from-purple-500 hover:to-blue-400  text-white font-bold rounded-full shadow-lg text-2xl transition duration-300 ease-in-out hover:scale-110">✓</button>
 
         </div>
           
